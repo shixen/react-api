d9ff7a5ccbab0980fcc5b7c6a93c4f3eda208609
Niklas Eriksson <niklaseriksson32@hotmail.com>
2024-08-04 11:38:21 +0000

    add image filter choise

---
+++ posts/serializers.py
@@ -0,0 +1,37 @@
+from rest_framework import serializers
+from posts.models import Post
+
+
+class PostSerializer(serializers.ModelsSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+
+    def validate_image(self, value):
+        if value.size > 1024 * 1024 * 2:
+            raise serializers.ValidationError(
+                'image size larger then 2MB!'
+            )
+        if value.image.width > 4096:
+            raise serializers.ValidationError(
+                'image width larger than 4096px'
+            )
+        if value.image.height > 4096:
+            raise serializers.ValidationError(
+                'image height larger than 4096px'
+            )
+        return value
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+
+    class Meta:
+        model = Post
+        fields = [
+            'id', 'owner', 'is_owner', 'profile_id',
+            'profile_image', 'created_at', 'updated_at',
+            'title', 'content', 'image', 'image_filter'
+        ]
+
