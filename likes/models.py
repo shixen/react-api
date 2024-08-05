96e272875ce13a5f687182073cc01f0d45459056
Niklas Eriksson <niklaseriksson32@hotmail.com>
2024-08-05 11:09:03 +0000

    add likes edit and delete

--- likes/models.py
+++ likes/models.py
@@ -2,18 +2,17 @@ from django.db import models
 from django.contrib.auth.models import User
 from posts.models import Post
 
-# Create your models here.
 
-class Likes(models.Model):
+class Like(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, related_name='likes', on_delete=models.CASCADE)
-    created_at = models.DateField(auto_now_add=True)
+    post = models.ForeignKey(
+        Post, related_name='likes', on_delete=models.CASCADE
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
 
-
-    class meta:
+    class Meta:
         ordering = ['-created_at']
         unique_together = ['owner', 'post']
 
-
     def __str__(self):
-        return f"{self.owner}{self.post}"
+        return f'{self.owner} {self.post}'
